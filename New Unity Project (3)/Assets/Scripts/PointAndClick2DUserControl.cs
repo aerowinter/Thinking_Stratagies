fafd149e47c3db196b977576a009cfc246272217
skyeUAT <tylsauls@uat.edu>
2019-11-21 20:41:00 -0700

    player & movement script
    very basic movement script w/ placeholder player

---
+++ New Unity Project (3)/Assets/Scripts/PointAndClick2DUserControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAndClick2DUserControl : MonoBehaviour
+{
+    public float speed = 4.5f;
+    private Vector2 target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.y = transform.position.y;
+        }
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+}
