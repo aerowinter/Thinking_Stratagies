0d4c17b4a1e71ff416b8baa232de3582b60d456c
aerowinter <55407143+aerowinter@users.noreply.github.com>
2019-11-25 01:34:13 -0700

    switching to a new inventory method i think will work better
    just the first step

---
+++ New Unity Project (3)/Assets/InteractionObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionObject : MonoBehaviour
+{
+  public void DoInteraction()
+    {
+        //picked up and put in inventory
+        gameObject.SetActive(false);
+    }
+}
---
+++ New Unity Project (3)/Assets/PlayerInteract.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    public GameObject currentInterObj = null;
+
+    void Update()
+    {
+     if(Input.GetButtonDown ("Interact") && currentInterObj){
+            //do something with object
+            currentInterObj.SendMessage("DoInteraction");
+        }  
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+       if(other.CompareTag ("interObject")) {
+            Debug.Log(other.name);
+            currentInterObj = other.gameObject;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other) {
+        if (other.CompareTag("interObject")) {
+            if (other.gameObject == currentInterObj) {
+                currentInterObj = null;
+            }
+            
+        }
+    }
+}
