b4f77aff6f5e29ce6ab1b13fe33b19893bbf2f4e
aerowinter <55407143+aerowinter@users.noreply.github.com>
2019-11-24 21:16:40 -0700

    UI and inventory

---
+++ New Unity Project (3)/Assets/Scripts/Inventory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    //to check if there is already an item in a given inventory slot
+    public bool[] isFull;
+    //parents a picked up item to a given inventory slot
+    public GameObject[] slots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
